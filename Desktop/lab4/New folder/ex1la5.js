e47d4e3740923340575a0e7b8db46087c2fc7a58
cs445 <79168610+eltohenok@users.noreply.github.com>
2021-05-26 16:15:36 -0700

    ex1lab5.js

---
+++ Desktop/lab4/New folder/ex1la5.js
@@ -0,0 +1,17 @@
+const isPrime = (num) => {
+    return new Promise(function(resolve, reject) {
+        setTimeout(() => {
+            for (let i = 2, s = Math.sqrt(num); i <= s; i++) {
+                if (num % i === 0) {
+                    reject({ prime: false });
+                }
+            }
+            resolve({ prime: num > 1 });
+        }, 500);
+    });
+};
+console.log('start');
+isPrime(7)
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
+console.log('end');
